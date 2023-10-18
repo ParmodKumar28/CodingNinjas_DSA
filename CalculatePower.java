861b0fb00da19b8887a2a6fdfc2a745826cc89d5
Parmod Kumar <yparmodyadav@gmail.com>
2023-10-18 10:59:22 +0530

    Milestone 2 Lecture 14 : Recursion 1 18/10/2023

---
+++ CalculatePower.java
@@ -0,0 +1,48 @@
+// Calculate Power
+// Send Feedback
+// Write a program to find x to the power n (i.e. x^n). Take x and n from the user. You need to return the answer.
+// Do this recursively.
+// Input format :
+// Two integers x and n (separated by space)
+// Output Format :
+// x^n (i.e. x raise to the power n)
+// Constraints :
+// 0 <= x <= 30
+// 0 <= n <= 30
+// Sample Input 1 :
+//  3 4
+// Sample Output 1 :
+// 81
+// Sample Input 2 :
+//  2 5
+// Sample Output 2 :
+// 32
+
+import java.util.Scanner;
+
+/**
+ * CalculatePower
+ */
+public class CalculatePower {
+    public static void main(String[] args) {
+        Scanner s =  new Scanner(System.in);
+        System.out.print("Enter x : ");
+        int x = s.nextInt();
+        System.out.print("Enter n : ");
+        int n = s.nextInt();
+        int ans = power(x, n);
+        System.out.println("Output : " + ans);
+        s.close();
+    }
+
+    public static int power(int x, int n)
+    {
+        if(n==0)
+        {
+            return 1;
+        }
+        return x * power(x, n-1);
+    }
+    
+}
+
