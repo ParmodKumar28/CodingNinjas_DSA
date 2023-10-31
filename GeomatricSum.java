e1ee33c6bc689d943f701726015180073a7166b4
Parmod Kumar <yparmodyadav@gmail.com>
2023-10-31 11:43:03 +0530

    Recursion Assignment Completed 31/10/2023

---
+++ GeomatricSum.java
@@ -0,0 +1,44 @@
+// Geometric Sum
+// Send Feedback
+// Given k, find the geometric sum i.e.
+// 1 + 1/2 + 1/4 + 1/8 + ... + 1/(2^k) 
+// using recursion.
+// Input format :
+// Integer k
+// Output format :
+// Geometric sum (upto 5 decimal places)
+// Constraints :
+// 0 <= k <= 1000
+// Sample Input 1 :
+// 3
+// Sample Output 1 :
+// 1.87500
+// Sample Input 2 :
+// 4
+// Sample Output 2 :
+// 1.93750
+// Explanation for Sample Input 1:
+// 1+ 1/(2^1) + 1/(2^2) + 1/(2^3) = 1.87500
+
+import java.util.Scanner;
+
+public class GeomatricSum {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Give Integer : ");
+        int k = s.nextInt();
+        double ans = findGeometricSum(k);
+        System.out.print("Output : " + ans);
+        s.close();
+    }
+
+    public static double findGeometricSum(int k) {
+        if (k == 0) {
+            return 1;
+        } else {
+            double a = 1 / (Math.pow(2, k));
+            return a + findGeometricSum(k - 1);
+        }
+
+    }
+}
